6bfea06e122db4757f93f48f91284fe6e88a104f
idietmoran <idietmoran@gmail.com>
2016-10-24 16:21:26 -0400

    1

--- client/js/options.js
+++ client/js/options.js
@@ -1,7 +1,36 @@
-$('#options-submit').on('click', function() {
-    console.log('submit');
+const Config = require('../../scripts/configController');
+const fs = require('fs');
+
+// TODO: set current config options to display
+
+// displays current config settings to page
+function dispCurrent() {
+
+}
+// on load display current
+Window.onLoad(function() {
+    dispCurrent();
 });
 
+$('#options-submit').on('click', function() {
+    // Read config
+    let config = require('../../data/config.json');
+
+    // Get our input values
+    let defaultPage = $('#options-page').value();
+    let player = $('#options-player').value();
+    let vlcPath = $('#options-vlc-path').value();
+    let chat = $('#options-chat').value();
+    let quality = $('#quality').value();
+});
+
+
+// Cancel all current options
 $('#options-cancel').on('click', function() {
-    console.log('cancel');
+    dispCurrent();
+});
+
+$('#options-default').on('click', function() {
+    //TODO: add alert window to confirm resetting to defaul
+    Config.default();
 });
